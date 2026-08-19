927c6ea7d1d09a8f56a39c2e61583d821ac45968
gabriel-wan <gabrielwanjp@gmail.com>
2026-08-19 10:04:45 +0800

    Implement Level-3 task completion

--- src/main/java/Sherlock.java
+++ src/main/java/Sherlock.java
@@ -20,7 +20,7 @@ public class Sherlock {
         System.out.println("Hello! I'm Sherlock, your detective assistant.");
         System.out.println("What can I do for you?");
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numberOfTasks = 0;
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
@@ -29,15 +29,71 @@ public class Sherlock {
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
                 } else if (command.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < numberOfTasks; i++) {
                         System.out.println((i + 1) + ". " + tasks[i]);
                     }
+                } else if (command.startsWith("mark ")) {
+                    int taskNumber = Integer.parseInt(command.substring(5));
+                    Task completedTask = tasks[taskNumber - 1];
+                    completedTask.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + completedTask);
+                } else if (command.startsWith("unmark ")) {
+                    int taskNumber = Integer.parseInt(command.substring(7));
+                    Task incompleteTask = tasks[taskNumber - 1];
+                    incompleteTask.markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + incompleteTask);
                 } else {
-                    tasks[numberOfTasks] = command;
+                    tasks[numberOfTasks] = new Task(command);
                     numberOfTasks++;
-                    System.out.println("added: " + command);
+                    System.out.println("added: " + tasks[numberOfTasks - 1]);
                 }
             }
         }
+    }
+}
+
+/**
+ * Represents one task and whether it has been completed.
+ */
+class Task {
+    private final String description;
+    private boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description text describing the task
+     */
+    Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as completed.
+     */
+    void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as incomplete.
+     */
+    void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the task in the text UI's list format.
+     *
+     * @return the completion status followed by the task description
+     */
+    @Override
+    public String toString() {
+        String statusIcon = isDone ? "X" : " ";
+        return "[" + statusIcon + "] " + description;
     }
 }
